b9b434b87b5aab7824a5a8d5e650ffa4fec5aa59
Aries <aho11@stuy.edu>
2020-10-29 18:52:35 -0400

    changed name from Triangles to Triangle

---
+++ Triangle.java
@@ -0,0 +1,36 @@
+public class Triangle{
+  private  Point v1, v2, v3;
+  //constructor
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+    v1 = Point(x1, y1);
+    v2 = Point(x2, y2);
+    v3 = Point(x3, y3);
+  }
+
+  public Triangle(Point a, Point b, Point c){
+    v1 = a;
+    v2 = b;
+    v3 = c;
+  }
+
+  //accessors
+  public double getPerimeter(){
+    return (v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1));
+  }
+
+  public double getArea(){
+
+  }
+
+  public String classify(){
+
+  }
+
+  public String toString(){
+
+  }
+  //mutator
+  public void setVertex(int index, Point newP){
+
+  }
+}
