bf420c5da8259c184fd45e642c18fa954ef03a8f
Aries <aho11@stuy.edu>
2020-10-29 18:26:43 -0400

    added equals method

--- Point.java
+++ Point.java
@@ -24,4 +24,8 @@ public class Point{
   public double distanceTo(Point other){
     return Math.sqrt((this.x - other.x)*(this.x - other.x) + (this.y - other.y)*(this.y - other.y));
   }
+
+  public boolean equals(Point other){
+    return ((this.x == other.x) && (this.y == other.y));
+  }
 }
