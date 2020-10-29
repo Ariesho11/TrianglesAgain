3fece3b3dcdbb1194f14c443e09c6a92b0e6f06a
Aries <aho11@stuy.edu>
2020-10-29 18:33:27 -0400

    Set up Triangles.java

---
+++ Triangles.java
@@ -0,0 +1,29 @@
+public class Triangles{
+  private  Point v1, v2, v3;
+  //constructor
+  public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
+    //v1 =
+    //v2 =
+    //v3 =
+  }
+  //accessors
+  public double getPerimeter(){
+
+  }
+
+  public double getArea(){
+
+  }
+
+  public String classify(){
+
+  }
+
+  public String toString(){
+
+  }
+  //mutator
+  public void setVertex(in index, Point newP){
+    
+  }
+}
