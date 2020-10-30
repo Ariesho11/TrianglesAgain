0d33ad574eed7c0c0c40cdc1f9112a45e05b62ef
Aries <aho11@stuy.edu>
2020-10-29 20:38:46 -0400

    added setVertx and fixed toString

--- Triangle.java
+++ Triangle.java
@@ -1,5 +1,5 @@
 public class Triangle{
-  private  Point v1, v2, v3;
+  private Point v1, v2, v3;
   //constructor
   public Triangle(double x1, double y1, double x2, double y2, double x3, double y3){
     v1 = new Point(x1, y1);
@@ -39,9 +39,12 @@ public class Triangle{
   }
 
   public String toString(){
-    return "v1(" + v1.x + ", " + v1.y + " v2(" + v1.x + ", " + v1.y + " v1(" + v1.x + ", " + v1.y;
+    return "v1(" + v1.getX() + ", " + v1.getY() + ") v2(" + v2.getX() + ", " + v2.getY() + ") v3(" + v3.getX() + ", " + v3.getY() + ")";
   }
   //mutator
   public void setVertex(int index, Point newP){
+    if (index == 0) v1 = newP;
+    if (index == 1) v2 = newP;
+    if (index == 2) v3 = newP;
   }
 }
